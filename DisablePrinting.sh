72074aeaecfdb363a9f07b55f669464e233fa601
bitjbullock <69867367+bitjbullock@users.noreply.github.com>
2023-11-23 09:16:38 -0500

    Create DisablePrinting.sh

---
+++ DisablePrinting.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# This is a bash script for MacOS. I know it's in WindowsAdmin repo but I don't write many MacOS scripts so here it goes. 
+# Written by Jonathan Bullock
+# 2023 - 11 - 23
+
+# Confirm running as root
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+# Stop the CUPS service
+echo "Stopping CUPS service..."
+cupsctl --no-share-printers
+
+# Disable CUPS service from starting at boot
+echo "Disabling CUPS service from starting at boot..."
+launchctl unload /System/Library/LaunchDaemons/org.cups.cupsd.plist
+
+# Optionally, remove all existing printers
+echo "Removing all existing printers..."
+lpstat -p | awk '{print $2}' | xargs -I {} lpadmin -x {}
+
+echo "Printing has been disabled on this device."
